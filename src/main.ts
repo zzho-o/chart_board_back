c5d5102bc6a53c019eb797200f7bb0ffd38a8f18
zzho0 <rkdwjsgh0@mxlab.kr>
2025-11-07 16:02:26 +0900

    feat:swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'node_modules', 'swagger-ui-dist'));
+
   app.enableCors({ origin: '*' });
 
   const config = new DocumentBuilder()
@@ -15,8 +20,15 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+      docExpansion: 'none',
+    },
+  });
 
   await app.listen(process.env.PORT || 3000);
+  console.log(`Swagger ready → https://chart-board-back.vercel.app/docs`);
 }
+
 bootstrap();
